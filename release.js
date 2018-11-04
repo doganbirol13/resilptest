d69e582762515c7c8fc9af1e763e959cd22ecb9e
doganbirol13 <doganbirol13@gmail.com>
2018-11-04 16:48:25 +0300

    SSL_Implemented

--- release.js
+++ release.js
@@ -4,12 +4,24 @@
 // https://www.totaljs.com
 // ===================================================
 
-const options = {};
+// const options = {};
 
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'Total.js' };
-// options.sleep = 3000;
+// // options.ip = '127.0.0.1';
+// // options.port = parseInt(process.argv[2]);
+// // options.config = { name: 'Total.js' };
+// // options.sleep = 3000;
 
-require('total.js').http('release', options);
-// require('total.js').cluster.http(5, 'release', options);
+// require('total.js').http('release', options);
+// // require('total.js').cluster.http(5, 'release', options);
+
+
+ar fs = require('fs');
+
+var options = {};
+options.https = {
+   pfx: fs.readFileSync('./SSL/resitestlp.pfx'),
+   passphrase: '12345'
+};
+options.port = 8000;
+
+require('total.js').https('release', options);
